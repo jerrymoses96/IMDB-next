be9962ec2a22bfd3a2e4fef2fd4b927473ee36b1
jerin <jerinkunnamkada@gmail.com>
2024-05-22 17:09:45 +0530

    edited

---
+++ src/app/about/page.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs. Our website is designed to provide you
+        with a comprehensive database of movies from all around the world, along
+        with the latest news, reviews, and trailers. Our movie database is
+        constantly updated with new releases, ensuring that you have access to
+        the latest and greatest in the world of cinema. You can search for
+        movies by title, director, actor, genre, or release date, making it easy
+        to find the perfect movie for any occasion.
+      </p>
+      <p></p>
+    </div>
+  );
+};
+
+export default about;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
       <div className="flex items-center gap-4">
         <DarkModeSwitch />
         <Link href={"/"} className="flex items-center gap-1 ">
-          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+          <span className="text-2xl font-bold bg-amber-500  py-1 px-2 rounded-lg">
             IMDB
           </span>
           <span className="text-xl hidden sm:inline ">clone</span>
