80ed755156492d1c616a5ba5ca841dcdd11dcf30
jerin <jerinkunnamkada@gmail.com>
2024-05-22 17:42:52 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarItem from "./NavbarItem";
+
+const Navbar = () => {
+  return (
+    <div>
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
+      
+    </div>
+  );
+};
+
+export default Navbar;
